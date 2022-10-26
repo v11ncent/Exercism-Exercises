806a0aea3fbf5cddb1a3ce89bdb2124909b0e396
Vince <vince321c@gmail.com>
2022-10-26 19:50:05 -0400

    Finished Matrix exercise

--- Matrix/Matrix.js
+++ Matrix/Matrix.js
@@ -1,7 +1,3 @@
-const string = '9 8 7\n5 3 2\n6 6 7\n';
-const stringMatrix = new Matrix(string);
-console.log(stringMatrix.getColumns);
-
 class Matrix {
     matrix = [];
     columns = [];
@@ -9,8 +5,8 @@ class Matrix {
     
     constructor(string) {
         this.setMatrix = string;
-        this.setColumns = this.matrix;
-        this.setRows = '';
+        this.setColumns = this.getMatrix;
+        this.setRows = this.getMatrix;
     }
 
     get getMatrix() {
@@ -44,6 +40,7 @@ class Matrix {
             matrixArray.push(innerArray);
         }
 
+
         this.matrix = matrixArray;
     }
 
@@ -51,16 +48,26 @@ class Matrix {
         let columnsArray = [];
 
         for (let i = 0; i < matrix.length; i++) {
-            columnsArray.push(matrix[i]);
             for (let j = 0; j < matrix.length; j++) {
-                columnsArray.push(matrix[j]);
+                columnsArray.push(matrix[j][i]);
             }
         }
 
         this.columns = columnsArray;
     }
 
-    set setRows(value) {
-        this.rows = value;
+    set setRows(matrix) {
+        let rowsArray = [];
+
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                rowsArray.push(matrix[i][j]);
+            }
+        }
+
+        this.rows = rowsArray;
     }
 }
+
+const stringMatrix = new Matrix('9 8 7\n5 3 2\n6 6 7\n');
+console.log(stringMatrix.getColumns);
---
+++ Matrix/README.md
@@ -0,0 +1,5 @@
+# Status
+Complete
+
+## Possible things to do
+Instead of a 2d array, I'd like to make the code work with a dynamic row/column length
